40e4a53e205e21a96bd69826e7853f2f36596e8f
Shreya Laddha <47536876+laddhashreya2000@users.noreply.github.com>
2020-08-29 03:09:48 +0530

    Add files via upload

---
+++ 2/code/myLinearContrastStretching.m
@@ -0,0 +1,20 @@
+function out_image = myLinearContrastStretching(in_image)
+    channels = size(in_image, 3);
+
+    out_image = zeros([size(in_image, 1), size(in_image, 2), size(in_image, 3)]);
+    for i = 1:channels
+        greyscale = in_image(:,:,i);
+        min_intensity = min(greyscale(:));
+        max_intensity = max(greyscale(:));
+        a = 255/(max_intensity - min_intensity);
+        b = 255 - (a*max_intensity);
+        out_image(:,:,i) = a*in_image(:,:, i) + b;
+    end
+    out_image = uint8(out_image);
+    myNumOfColors = 200;
+    myColorScale = [ [0:1/(myNumOfColors-1):1]',[0:1/(myNumOfColors-1):1]',[0:1/(myNumOfColors-1):1]' ];
+    subplot(1,2,1), imagesc(in_image); colormap (myColorScale);colormap gray;
+    daspect ([1 1 1]); axis tight; colorbar;
+    subplot(1,2,2); imagesc(out_image); colormap (myColorScale);colormap gray;
+    daspect ([1 1 1]); axis tight; colorbar; 
+end
