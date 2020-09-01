f9ced474b197ca1523a7d238d055e4b345571ed6
Shreya Laddha <47536876+laddhashreya2000@users.noreply.github.com>
2020-09-01 19:28:18 +0530

    Update myLinearContrastStretching.m

--- 2/code/myLinearContrastStretching.m
+++ 2/code/myLinearContrastStretching.m
@@ -11,10 +11,5 @@ function out_image = myLinearContrastStretching(in_image)
         out_image(:,:,i) = a*in_image(:,:, i) + b;
     end
     out_image = uint8(out_image);
-    myNumOfColors = 200;
-    myColorScale = [ [0:1/(myNumOfColors-1):1]',[0:1/(myNumOfColors-1):1]',[0:1/(myNumOfColors-1):1]' ];
-    subplot(1,2,1), imagesc(in_image); colormap (myColorScale);colormap gray;
-    daspect ([1 1 1]); axis tight; colorbar;
-    subplot(1,2,2); imagesc(out_image); colormap (myColorScale);colormap gray;
-    daspect ([1 1 1]); axis tight; colorbar; 
+
 end
