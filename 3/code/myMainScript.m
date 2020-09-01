0e838bdae50ff89c873b8750158286252d9fcd5d
arya2331 <rishabharya32@gmail.com>
2020-09-01 21:14:56 +0530

    added mainscript for ques3

--- 3/code/myMainScript.m
+++ 3/code/myMainScript.m
@@ -1,6 +1,18 @@
-%% MyMainScript
+%%MymainScript
 
 tic;
-%% Your code here
+
+% Your code here 
+
+myNumOfColors = 256; 
+colorScale = 0:1/(myNumOfColors-1):1;
+myColorScale = [ colorScale' colorScale' colorScale' ];
+
+img = imread('../data/test.jpg');
+he_img = myHE(img);
+myBiHistogramEqualization1=myBiHistogramEqualization(img);
+figure, image(img),  title('Test Image'),colormap(myColorScale), colorbar, daspect([1 1 1]);
+figure, image(he_img),  title('Histogram Eqalized image'),colormap(myColorScale), colorbar, daspect([1 1 1]);
+figure, image(myBiHistogramEqualization1),  title('Bi-Histogram equalized image'),colormap(myColorScale), colorbar, daspect([1 1 1]);
 
 toc;
