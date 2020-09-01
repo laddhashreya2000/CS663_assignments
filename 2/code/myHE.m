f87849ed0ef412a5ef3c4e94ee7f598d88eba8dc
arya2331 <rishabharya32@gmail.com>
2020-09-01 18:46:05 +0530

    corrected myHM.m

--- 2/code/myHE.m
+++ 2/code/myHE.m
@@ -1,49 +1,24 @@
 function out_image = myHE(in_image)
     [X,Y,Z] = size(in_image);
-    in_image1 = in_image(:,:,1);
-    in_image2 = in_image(:,:,2);
-    in_image3 = in_image(:,:,3);
-    out_image1 = zeros(X,Y); out_image2 = zeros(X,Y); out_image3 = zeros(X,Y);
 
-    hist = zeros(1,256); cdf = zeros(1,256);
-    for j = 0:255
-        hist(j+1) = sum(sum(in_image1==j))/(X*Y); 
-    end
-    for j = 1:256
-        cdf(j) = sum(hist(1:j));
-    end
-    for j = 1:X
-        for k = 1:Y
-            out_image1(j,k) = ceil(255*cdf(in_image1(j,k)+1));
+    for i= 1:Z
+        in_img= in_image(:,:,i);
+        out_img=zeros(X,Y);
+        hist = zeros(1,256); cdf = zeros(1,256);
+        for j = 0:255
+            hist(j+1) = sum(sum(in_img==j))/(X*Y); 
         end
-    end
-
-    hist = zeros(1,256); cdf = zeros(1,256);
-    for j = 0:255
-        hist(j+1) = sum(sum(in_image2==j))/(X*Y); 
-    end
-    for j = 1:256
-        cdf(j) = sum(hist(1:j));
-    end
-    for j = 1:X
-        for k = 1:Y
-            out_image2(j,k) = ceil(255*cdf(in_image2(j,k)+1));
+        for j = 1:256
+            cdf(j) = sum(hist(1:j));
         end
-    end
-
-    hist = zeros(1,256); cdf = zeros(1,256);
-    for j = 0:255
-        hist(j+1) = sum(sum(in_image3==j))/(X*Y); 
-    end
-    for j = 1:256
-        cdf(j) = sum(hist(1:j));
-    end
-    for j = 1:X
-        for k = 1:Y
-            out_image3(j,k) = ceil(255*cdf(in_image3(j,k)+1));
+        for j = 1:X
+            for k = 1:Y
+                out_img(j,k) = ceil(255*cdf(in_img(j,k)+1));
+            end
         end
+        out_image(:,:,i)=out_img;
     end
-    out_image(:,:,1)=out_image1;out_image(:,:,2)=out_image2;out_image(:,:,3)=out_image3;
+    
     out_image = uint8(out_image);
     subplot(1,2,1); imshow(in_image);
     subplot(1,2,2); imshow(out_image);  
