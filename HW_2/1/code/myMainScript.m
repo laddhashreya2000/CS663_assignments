29495a19b3e476ad4c335a87615e2ffb7774192b
ShreyanJabade <58798198+ShreyanJabade@users.noreply.github.com>
2020-09-28 15:40:37 +0530

    Update myMainScript.m

--- HW_2/1/code/myMainScript.m
+++ HW_2/1/code/myMainScript.m
@@ -3,4 +3,12 @@
 tic;
 %% Your code here
 
+img1 = load('../data/superMoonCrop.mat');
+img2 = load('../data/lionCrop.mat');
+img1 = img1.imageOrig;
+img2 = img2.imageOrig;
+
+out1=myUnsharpMasking(img1, 1.5, 5);
+out2=myUnsharpMasking(img2, 1 , 3);
+
 toc;
