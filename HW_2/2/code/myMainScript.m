9b311717529a41d1ccec5f6f586ccbda6518af80
ShreyanJabade <58798198+ShreyanJabade@users.noreply.github.com>
2020-09-28 18:15:05 +0530

    Update myMainScript.m

--- HW_2/2/code/myMainScript.m
+++ HW_2/2/code/myMainScript.m
@@ -3,4 +3,17 @@
 tic;
 %% Your code here
 
+img1 = load('../data/barbara.mat');
+img1 = img1.imageOrig;
+
+img2=imread('../data/grass.png');
+
+img3=imread('../data/honeyCombReal.png');
+
+[out1, RMSD1]=myBilateralFiltering(img1, 0.5, 0.1);
+
+[out2, RMSD2]=myBilateralFiltering(img2, 1.428, 0.1);
+
+[out3, RMSD3]=myBilateralFiltering(img3, 0.75, 0.2);
+
 toc;
