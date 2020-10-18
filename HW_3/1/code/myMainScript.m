0d1c2a5ec91885895d63b9c73c974dc13a99a640
ShreyanJabade <58798198+ShreyanJabade@users.noreply.github.com>
2020-10-18 17:15:30 +0530

    Update myMainScript.m

--- HW_3/1/code/myMainScript.m
+++ HW_3/1/code/myMainScript.m
@@ -3,7 +3,7 @@
 tic;
 %% Your code here
 
-in_image = load('boat'); in_image = in_image.imageOrig;
+in_image = load('../data/boat.mat'); in_image = in_image.imageOrig;
 cor_image = myHarrisCornerDetector(in_image, 0.5, 3, 0.04);
 
 toc;
